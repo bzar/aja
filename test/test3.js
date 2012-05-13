0ebf8b751ed4c2caccc74aae945ca128dd94fcdc
Teemu Erkkola <teemu.erkkola@iki.fi>
2012-05-13 22:35:56 +0300

    Added Effect and OpacityEffect

--- test/test3.js
+++ test/test3.js
@@ -5,18 +5,16 @@ window.onload = function() {
 
   var image = new aja.Image("infantry.png");
 
-  function moveDownAndRedraw() {
-    image.y += 50;
-    canvas.redrawEntity(image);
-  };
+  image.effects = [new aja.OpacityEffect];
+  image.opacity = 1.0;
   
   var animation = new aja.SequentialAnimation([
     new aja.PropertyAnimation(image, {x: 200}, 1000),
     new aja.PropertyAnimation(image, {x: 0, y: 50}, 1000),
     new aja.PropertyAnimation(image, {x: {delta: 100}}, 1000),
     new aja.PropertyAnimation(image, {x: {to: 300}}, 1000),
-    new aja.PropertyAnimation(image, {y: 200}, 1000),
-    new aja.PropertyAnimation(image, {x: {from: 0, to: 300}}, 1000)
+    new aja.PropertyAnimation(image, {y: 200, opacity: 0.0}, 1000),
+    new aja.PropertyAnimation(image, {x: {from: 0, to: 300}, opacity: 1.0}, 1000)
   ]);
   
   canvas.addEntity(image);
